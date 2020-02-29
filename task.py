d90a5224b4fe630f0feb7d26dc52733cdc55d20b
Matthew Kerr <kerrmat@oregonstate.edu>
2020-02-29 13:25:08 -0800

    Added circle area calculator

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
+import math
 
 
 def firstrun():
     return "success"
+    
+def computearea(radius):
+    return math.pow(radius, 2) * math.pi
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -11,6 +12,16 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def test3(self):
+        radius = 2
+        expected = math.pi * math.pow(radius, 2)
+        self.assertEqual(expected, task.computearea(radius))
+
+    def test4(self):
+        radius = 2
+        expected = 6
+        self.assertNotEqual(expected, task.computearea(radius))
 
 
 if __name__ == '__main__':
